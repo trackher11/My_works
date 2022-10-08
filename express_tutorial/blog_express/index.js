b7db136f19d25906afef1415341fb66a45099a3c
Rippling <hsahsik@gmail.com>
2022-10-08 20:10:17 +0530

    oct 8

---
+++ express_tutorial/blog_express/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+// const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "static")));
+app.use("/", require(path.join(__dirname, "routes/blog.js")));
+
+app.listen(port, () => {
+  console.log(`blogs app listening on port ${port}`);
+});
